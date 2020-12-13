9cb0863565b6d137bbd58a89b683ecb52dfd7ef2
PaulVasileMiron <55735548+PaulVasileMiron@users.noreply.github.com>
2020-12-13 23:37:11 +0100

    Add files via upload

--- js/videoOnline.js
+++ js/videoOnline.js
@@ -4,10 +4,8 @@ function CambiaVideo() { /*Para cambiar el video */
     console.log("Hola");
     if (contador == 0) {
         var video = $("#demo").attr("src", "C:/Users/User/Downloads/videoOnline.mp4");
-        console.log("Hola2");
     } else {
         var video = $("#demo").attr("src", "C:/Users/User/Downloads/videoOnline (" + contador + ").mp4");
-        console.log("Hola3");
     }
     contador++;
 }
