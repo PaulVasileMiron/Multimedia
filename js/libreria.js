30b82ff57f104121efa68a187d26813a566477f3
PaulVasileMiron <55735548+PaulVasileMiron@users.noreply.github.com>
2020-12-13 12:15:02 +0100

    Add files via upload

--- js/libreria.js
+++ js/libreria.js
@@ -5,6 +5,8 @@ function CambiaVideo(idVideo){ /*Para cambiar el video con los botones de la lis
 $(document).ready(function() { //Cargamos primero la pagina y los videos
 
 var preguntaRespondida=false;//video interactivo 2
+var preguntaRespondidav2 =false;
+var preguntaRespondidav3 =false;
 var preguntaRespondida1=false;//video interactivo 3
 
 function compruebaTiempo() {/*Funciones interactivas de los videos*/
@@ -12,7 +14,8 @@ function compruebaTiempo() {/*Funciones interactivas de los videos*/
   var video = document.getElementById('video');
 
   if($(".video").get(0).currentSrc.includes("1-interactivo")){ //Interacion Video Interactivo1
-
+    $('.botones').removeAttr('disabled')
+    $(".textoverlay").css("display", "block")
     if(video.currentTime>=3 && video.currentTime<7){
       $(".textoverlay").html("Los erizos son pequeños mamíferos cubiertos de púas");
     }else if(video.currentTime>=28 && video.currentTime<32){
@@ -35,14 +38,17 @@ function compruebaTiempo() {/*Funciones interactivas de los videos*/
     if(video.currentTime>=18 && video.currentTime<24 ){
       if(!preguntaRespondida){
         video.pause();
+        $(".textoverlay").css("display", "block")
         $(".textoverlay").html("¿Que fruta es?");
         $(".botonPantalla").css("display","block");
+        $('.botones').attr('disabled', 'disabled')
       }
       $("#respuesta2").on('click', function(event){ //El evento se produce cuando pulsa el boton correcto
         event.stopPropagation();
         event.stopImmediatePropagation();
        $(".botonPantalla").css("display","none");
        $(".textoverlay").html("Has pulsando respuesta correcta");
+       $('.botones').removeAttr('disabled')
        video.play();
        preguntaRespondida=true;
     });
@@ -52,22 +58,97 @@ function compruebaTiempo() {/*Funciones interactivas de los videos*/
         event.stopImmediatePropagation();
        $(".botonPantalla").css("display","none");
        $(".textoverlay").html("Has pulsando respuesta INCORRECTA");
+       $('.botones').removeAttr('disabled')
        video.play();
        preguntaRespondida=true;
     });
     }else{
       $(".textoverlay").html("");
+      $(".textoverlay").css("display", "none")
       preguntaRespondida=false;
     }
+
+    if(video.currentTime>=48 && video.currentTime<54 ){
+      if(!preguntaRespondidav2){
+        video.pause();
+        $(".textoverlay2").css("display", "block")
+        $(".textoverlay2").html("¿De que color es la fruta?");
+        $(".botonPantalla2").css("display","block");
+        $('.botones').attr('disabled', 'disabled')
+      }
+      $("#respuesta5video2").on('click', function(event){ //El evento se produce cuando pulsa el boton correcto
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+       $(".botonPantalla2").css("display","none");
+ 
+       $(".textoverlay2").html("Has pulsando respuesta correcta");
+       $('.botones').removeAttr('disabled')
+       video.play();
+       preguntaRespondidav2=true;
+    });
+    
+    $("#respuesta6,#respuesta7,#respuesta8").on('click', function(event){ //El evento se produce cuando no pulsa el boton correcto
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+       $(".botonPantalla2").css("display","none");
+       $(".textoverlay2").html("Has pulsando respuesta INCORRECTA");
+       $('.botones').removeAttr('disabled')
+       video.play();
+       preguntaRespondidav2=true;
+    });
+    }else{
+      $(".textoverlay2").html("");
+      $(".textoverlay2").css("display", "none")
+      preguntaRespondidav2=false;
+    }
+
+    if(video.currentTime>=78 && video.currentTime<84 ){
+      if(!preguntaRespondidav3){
+        video.pause();
+        $(".textoverlay3").css("display", "block")
+        $(".textoverlay3").html("¿Que contiene esta fruta?");
+        $(".botonPantalla3").css("display","block");
+        $('.botones').attr('disabled', 'disabled')
+      }
+      $("#respuesta12").on('click', function(event){ //El evento se produce cuando pulsa el boton correcto
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+       $(".botonPantalla3").css("display","none");
+ 
+       $(".textoverlay3").html("Has pulsando respuesta correcta");
+       $('.botones').removeAttr('disabled')
+       video.play();
+       preguntaRespondidav3=true;
+    });
+    
+    $("#respuesta9,#respuesta10,#respuesta11").on('click', function(event){ //El evento se produce cuando no pulsa el boton correcto
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+       $(".botonPantalla3").css("display","none");
+       $(".textoverlay3").html("Has pulsando respuesta INCORRECTA");
+       $('.botones').removeAttr('disabled')
+       video.play();
+       preguntaRespondidav3=true;
+    });
+    }else{
+      $(".textoverlay3").html("");
+      $(".textoverlay3").css("display", "none")
+      preguntaRespondidav3=false;
+    }
+
+
   }else{
     $(".botonPantalla").css("display","none");
   }
+
+  
   
 
   if($(".video").get(0).currentSrc.includes("3-interactivo")){ //Interacion Video Interactivo3
     if(video.currentTime>=18 && video.currentTime<21){
       if(!preguntaRespondida1){
         video.pause();
+        $(".textoverlay").css("display", "block")
         $(".textoverlay").html("¿De que color es el Kayak de la izquierda?");
         $(".botonRespuesta").css("display","block");
       }
@@ -86,7 +167,18 @@ function compruebaTiempo() {/*Funciones interactivas de los videos*/
 
     }else{
       $(".textoverlay").html("");
+      $(".textoverlay").css("display", "none")
       preguntaRespondida1=false;
+    }
+
+    if(video.currentTime>=30 && video.currentTime<35){
+      $(".imagenKayak").css("display","block");
+      $(".textoReserva").css("display", "block")
+      $(".textoReserva").html("¿Quieres probar?"); 
+    }else{
+      $(".imagenKayak").css("display","none");
+      $(".textoReserva").css("display", "none")
+      $(".textoReserva").html("");
     }
 
     if(video.currentTime>=5 && video.currentTime<9){
