1bd62367a5c40979e583f646fb9c762858e1fee2
PaulVasileMiron <55735548+PaulVasileMiron@users.noreply.github.com>
2020-12-13 23:36:38 +0100

    Add files via upload

--- js/videoOnline.js
+++ js/videoOnline.js
@@ -1,17 +1,13 @@
-function CambiaVideo(){ /*Para cambiar el video */
-    var video=$("#demo").attr("src","C:/Users/User/Downloads/videoOnline.mp4");
+contador = 0;
+
+function CambiaVideo() { /*Para cambiar el video */
+    console.log("Hola");
+    if (contador == 0) {
+        var video = $("#demo").attr("src", "C:/Users/User/Downloads/videoOnline.mp4");
+        console.log("Hola2");
+    } else {
+        var video = $("#demo").attr("src", "C:/Users/User/Downloads/videoOnline (" + contador + ").mp4");
+        console.log("Hola3");
+    }
+    contador++;
 }
-
-// $(document).ready(function() {
-//     var existe=true;
-//     function CambiaVideo(){ /*Para cambiar el video */
-//         if($(".demo").get(0).currentSrc.includes("videoOnline") && existe){
-//             var video=$("#demo").attr("src","C:/Users/User/Downloads/videoOnline.mp4");
-//             existe=false;
-//         }
-//         //"C:/Users/User/Desktop/online/videoOnline.mp4"
-//         //console.log(video);
-//     }
-//     var timer = setInterval(CambiaVideo, 1000);
-// });
-
